0fede60911cba3d0e1ae316a96f258ad71fc0a9d
Tyler Quach <tylerquach@Tylers-MacBook-Pro.local>
2025-08-10 14:12:41 -0700

    Save work before pulling

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <RNCamera
+        style={styles.camera}
+        type={RNCamera.Constants.Type.back}
+        captureAudio={false}
+      />
     </View>
   );
 }
@@ -23,6 +18,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  camera: {
+    flex: 1,
+  },
 });
-
-export default App;
